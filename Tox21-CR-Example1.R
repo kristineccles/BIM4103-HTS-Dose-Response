ad36c6b8a099213ed8e07cfde58f469625b3748a
Kristin Eccles <kristin.eccles@gmail.com>
2024-02-05 23:12:59 -0500

    updated class excercise

--- Tox21-CR-Example1.R
+++ Tox21-CR-Example1.R
@@ -19,52 +19,74 @@ library(ggplot2)
 
 # load data
 thyroid_df <- read.csv("tox21_thyroid.csv")
-# keep only the needed columns in r
-thyroid_df <- 
 
 ##########################################################################################
+#### Data Exploration ####
+# Explore the TPO assays data - plot example
+p1 <- ggplot(data = subset(thyroid_df, call == "Active") , aes(x=chemName, y=log10(ac50), color = assay))+
+  geom_point()+
+  scale_x_discrete(guide = guide_axis(angle = 90)) +
+  theme_minimal()+
+  theme(text=element_text(size=8))+
+  labs(x= "AC50", y="Chemical Name")
+p1
+
+#### Example 1 ####
 #### Prepare Data ####
-# parse data for excercise - e.g. chemical with the Bisphenol A
-thyroid_subset <- subset(thyroid_df, chemName == "Bisphenol A")
+# parse data for exercise - e.g. chemical with the Bisphenol B
+thyroid_AUR_BPB <- subset(thyroid_df, chemName == "Bisphenol B" & assay == "CCTE_Simmons_AUR_TPO_dn")
 
 #isolate the dose-response
-thyroid_dr <-  thyroid_subset$Concentration.Response..Index.Concentration.Response.
+thyroid_dr_AUR_BPB <-  thyroid_AUR_BPB$Concentration.Response..Index.Concentration.Response.
 
 # Remove parentheses and split by commas
-ranges <- strsplit(gsub("\\(|\\)", "", thyroid_dr), ",")
+ranges_AUR_BPB <- strsplit(gsub("\\(|\\)", "", thyroid_dr_AUR_BPB), ",")
 
 # Initialize lists to store results
-table_data <- list()
+table_data_AUR_BPB <- list()
 
 # Loop through each range and extract values
-for (range in ranges[[1]]) {
-  values <- as.numeric(unlist(strsplit(range, ":")))
-  table_data <- c(table_data, values)
+for (range in ranges_AUR_BPB[[1]]) {
+  values_AUR_BPB <- as.numeric(unlist(strsplit(range, ":")))
+  table_data_AUR_BPB <- c(table_data_AUR_BPB, values)
 }
 
 # Convert the list to a matrix
-num_cols <- length(values)
-table_matrix <- matrix(unlist(table_data), ncol = num_cols, byrow = TRUE)
+num_cols_AUR_BPB <- length(values_AUR_BPB)
+table_matrix_AUR_BPB <- matrix(unlist(table_data_AUR_BPB), ncol = num_cols, byrow = TRUE)
 # Convert the matrix to a data frame
-result_df <- as.data.frame(table_matrix)
+result_AUR_BPB <- as.data.frame(table_matrix_AUR_BPB)
 # Set column names
-colnames(result_df) <- c("rep", "concentration", "response")
-result_df
+colnames(result_AUR_BPB) <- c("rep", "concentration", "response")
+result_AUR_BPB
 
-##########################################################################################
 ##### Dose Response Modelling ####
 # fit for mixtures modeling
-individual_model<- drm(response~concentration, data=result_df,
-                       fct=LL.4(fixed=c(NA, NA , NA, NA), 
-                                names = c("Slope", "Lower Limit", "Upper Limit", "ED50")))
-summary(individual_model)
+model_AUR_BPB<- drm(response~concentration, data=result_AUR_BPB,
+                       fct=LL.4(fixed=c(NA, 0 , NA, NA), 
+                                names = c("Slope", "Lower Limit", "Upper Limit", "AC50")))
+summary(model_AUR_BPB)
 # Quick Plot of curve fits
-plot(individual_model)
+plot(model_AUR_BPB)
 
 # get coefficients
-individual_model_wCI <- tidy(individual_model, conf.int = TRUE)
+model_AUR_BPB_model_wCI <- tidy(model_AUR_BPB, conf.int = TRUE)
 # add 95% CI
-individual_model_wCI$CI95 <- individual_model_wCI$std.error *1.96
-individual_model_wCI
+model_AUR_BPB_model_wCI$CI95 <- model_AUR_BPB_model_wCI$std.error *1.96
+model_AUR_BPB_model_wCI
 
+# Question 1: Compare your drc predicted AC50 value to the tcpl derived AC50 value? 
+# Are the values the same? How can you tell?
+
+##########################################################################################
+#### Example 2 ####
+# Estimate the AC50 value for Bisphenol B in the CCTE_Simmons_GUA_TPO_dn using sample code in the example above
+
+
+# Question 2: Compare your drc predicted AC50 value to the tcpl derived AC50 value? 
+# Are the values the same? How can you tell?
+
+# Question 3: Compare the AC50 measures between the CCTE_Simmons_GUA_TPO_dn
+# and the CCTE_Simmons_AUR_TPO_dn assays. Both assays inform on similar the same molecular target.
+# Which AC50 value would you use for conducting a risk assessment and why?
 
